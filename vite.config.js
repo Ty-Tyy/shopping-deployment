d90a8720b9371c496879acf5daf5ecda69a85fdf
TyTy <chhaytyty12@gmail.com>
2024-11-22 16:52:20 +0700

    Remove MultiSelect component import and react from vendor chunk in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks: {
-          vendor: ['react', 'vue'], // Specify libraries you want in separate chunks
+          vendor: ['vue'], // Specify libraries you want in separate chunks
         }
       }
     }
